f17bc3469241dbc75873a0a97dbc847297388b28
Elías Lizano <josueeliaslizano@gmail.com>
2025-05-05 15:23:50 -0600

    Respaldo CSV creado + cambios varios.
    Se creo el sistema de reespaldo de el reporte html, donde se reespalda en un documento .csv . Se corrigieron errores en la generación de notas y se redondearon todos los decimales amplios para un estandar de un solo dígito decimal.

--- TP1/reporteHTML.py
+++ TP1/reporteHTML.py
@@ -13,8 +13,11 @@ def crearReporte(bD):
     reprobados = 0
     cantidadEstudiantes = len(bD)
     reporte = open('reporteHTML.html','w', encoding='utf-8')
-    reporte.write('<html>\n')
-    reporte.write('<head>\n')
+    respaldo = open('respaldoCSV.csv', 'w')
+    respaldo.write('Reporte en HTML Tarea Programada #1 \n')
+    respaldo.write('Detalle de notas. \n')
+    reporte.write('<html>\n') 
+    reporte.write('<head>\n') 
     reporte.write('<title>Reporte en HTML, Tarea Programada #1</title>\n')
     reporte.write('<style>\n')
     reporte.write('    body {\n')
@@ -42,18 +45,18 @@ def crearReporte(bD):
     reporte.write('<h1>Reporte en HTML, Tarea Programada #1</h1>\n')
     reporte.write("<table border='1'>\n")
     reporte.write('<tr>\n')
-    reporte.write('<th colspan="8">Detalle de notas.</th>\n')
+    reporte.write('<th colspan="7">Detalle de notas.</th>\n')
     reporte.write('</tr>\n')
     reporte.write('<tr>\n')
     reporte.write('<th>Nombre</th>\n')
-    reporte.write('<th>Apellido 1</th>\n')
-    reporte.write('<th>Apellido 2</th>\n')
+    reporte.write('<th>Apellidos</th>\n')
     reporte.write('<th>Género</th>\n')
     reporte.write('<th>Carné</th>\n')
     reporte.write('<th>Correo</th>\n')
     reporte.write('<th>Notas</th>\n')
     reporte.write('<th>Estado</th>\n')
     reporte.write('</tr>\n')
+    respaldo.write('Nombre,Apellidos,Género,Carné,Correo,Notas,Estado \n')
     for i in bD:
         nombre = i[0][0]
         ap1 = i[0][1]
@@ -76,17 +79,22 @@ def crearReporte(bD):
         else:
             genero = 'Femenino'
         reporte.write('<tr>\n')
-        reporte.write(f'<td>{nombre}</td><td>{ap1}</td><td>{ap2}</td><td>{genero}</td><td>{carne}</td><td>{correo}</td>' \
+        reporte.write(f'<td>{nombre}</td><td>{ap1} {ap2}</td><td>{genero}</td><td>{carne}</td><td>{correo}</td>' \
                       f'<td>{notas}</td><td>{resultado}</td>\n')
         reporte.write('</tr>\n')
+        respaldo.write(f'{nombre},{ap1} {ap2},{genero},{carne},{correo},{notas[0]} {notas[1]} {notas[2]} {notas[3]},{resultado} \n')
     porApro = ((aprobados/cantidadEstudiantes) *100)
+    porApro = round(porApro, 1)
     porRepro = ((reprobados/cantidadEstudiantes) *100)
+    porRepro = round(porRepro, 1)
     porRepo = ((reposicion/cantidadEstudiantes) *100)
+    porRepo = round(porRepo, 1)
     reporte.write('<tr>\n')
-    reporte.write(f'<th colspan="8">La base de datos posee {cantidadEstudiantes} estudiantes, de los cuales hay: {aprobados} ' \
+    reporte.write(f'<th colspan="7">La base de datos posee {cantidadEstudiantes} estudiantes, de los cuales hay: {aprobados} ' \
                   f'aprobados para un {porApro}%, {reposicion} a reposición para un {porRepo}%, {reprobados} reprobados para un {porRepro}%.</th>\n')
     reporte.write('</tr>\n')
     reporte.write('</table>\n')
     reporte.write('</body>\n')
     reporte.write('</html>')
     reporte.close
+    print(f'Su reporte se genero satisfactoriamente.')
