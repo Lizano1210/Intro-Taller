df96e1d1af939de805215dbc09d48feeec291174
Elías Lizano <josueeliaslizano@gmail.com>
2025-05-05 12:59:39 -0600

    HTML terminado.
    Se agrego parráfo final y se comprobo su correcto funcionamiento.

--- TP1/reporteHTML.py
+++ TP1/reporteHTML.py
@@ -8,6 +8,10 @@ import BDdinamica
 #bD = BDdinamica.bdDinamicaES() 
 
 def crearReporte(bD):
+    aprobados = 0
+    reposicion = 0
+    reprobados = 0
+    cantidadEstudiantes = len(bD)
     reporte = open('reporteHTML.html','w', encoding='utf-8')
     reporte.write('<html>\n')
     reporte.write('<head>\n')
@@ -60,10 +64,13 @@ def crearReporte(bD):
         notaFinal = i[4][3]
         if notaFinal >= 70:
             resultado = 'Aprobado'
+            aprobados += 1
         elif notaFinal >= 60:
             resultado = 'Reposición'
-        else:
+            reposicion += 1
+        elif notaFinal < 60:
             resultado = 'Reprobado'
+            reprobados += 1
         if i[1] == True:
          genero = 'Masculino'
         else:
@@ -72,6 +79,13 @@ def crearReporte(bD):
         reporte.write(f'<td>{nombre}</td><td>{ap1}</td><td>{ap2}</td><td>{genero}</td><td>{carne}</td><td>{correo}</td>' \
                       f'<td>{notas}</td><td>{resultado}</td>\n')
         reporte.write('</tr>\n')
+    porApro = ((aprobados/cantidadEstudiantes) *100)
+    porRepro = ((reprobados/cantidadEstudiantes) *100)
+    porRepo = ((reposicion/cantidadEstudiantes) *100)
+    reporte.write('<tr>\n')
+    reporte.write(f'<th colspan="8">La base de datos posee {cantidadEstudiantes} estudiantes, de los cuales hay: {aprobados} ' \
+                  f'aprobados para un {porApro}%, {reposicion} a reposición para un {porRepo}%, {reprobados} reprobados para un {porRepro}%.</th>\n')
+    reporte.write('</tr>\n')
     reporte.write('</table>\n')
     reporte.write('</body>\n')
     reporte.write('</html>')
